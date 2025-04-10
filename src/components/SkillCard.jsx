6c7bc3bb7bf2f9c6f22ca1a489734693c98a745a
Gary-Bull <garydena12@gmail.com>
2025-04-10 18:50:46 +0100

    changed the skill card to only be a container with an icon in it also added some framer motion for animation

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SkillCard = ({ icon, skillName, progress, description }) => {
+import { AnimatePresence, motion } from "framer-motion";
+
+const SkillCard = ({ icon, skillName }) => {
+  const [showOverlay, setShowOverlay] = useState(false);
+
   return (
-    <div className="bg-white rounded-2xl border border-cyan-500 p-4">
-      <div className="flex gap-3">
-        <div className="w-10 h-10 flex items-center justify-center bg-linear-to-t from-cyan-100 to-[#ffffff] rounded-[7px]">
-          {icon}
-        </div>
-        <div className="flex-1">
-          <div className="flex items-center justify-between">
-            <p className="text-[13px] text-black font-medium">{skillName}</p>
-            <p className="text-xs text-blue-500 font-medium">{progress}%</p>
-          </div>
-          <div className="w-full bg-[#ECFAFCFF] rounded-md h-[5px] relative mt-2">
-            <div
-              className="bg-blue-500 h-[5px] rounded-md"
-              style={{ width: `${progress}%` }}
-            ></div>
-          </div>
-        </div>
-      </div>
-      <p className="text-xs text-gray-600 text-justify leading-5 mt-2">
-        {description}
-      </p>
+    <div className="bg-linear-to-t from-cyan-400 to-blue-400 rounded-2xl">
+      <motion.div
+        className="relative w-36 h-36 flex gap-1 justify-center items-center"
+        onHoverStart={() => setShowOverlay(true)}
+        onHoverEnd={() => setShowOverlay(false)}
+      >
+        <div className="flex items-center justify-center">{icon}</div>
+        <AnimatePresence>
+          {showOverlay && (
+            <motion.div
+              className="absolute inset-0 z-10 flex justify-center items-center"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <div className="absolute bg-blue-900 rounded-2xl pointer-events-none opacity-50 h-full w-full" />
+              <motion.h1
+                className="bg-white font-semibold text-sm z-10 px-3 py-2 rounded-full flex items-center gap-[0.5ch]"
+                initial={{ y: 10 }}
+                animate={{ y: 0 }}
+                exit={{ y: 0 }}
+              >
+                <span className="text-blue-600">{skillName}</span>
+              </motion.h1>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 };
